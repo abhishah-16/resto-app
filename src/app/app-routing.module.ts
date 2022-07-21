f91c4e42a4d5561150e44670af8e1262b4d73784
abhishah <abhishah0961@gmail.com>
2022-07-21 11:54:49 +0530

    [delete] delete component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddRestoComponent } from './add-resto/add-resto.component';
 import { ListRestoComponent } from './list-resto/list-resto.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { UpdateRestoComponent } from './update-resto/update-resto.component';
 
 const routes: Routes = [
@@ -14,14 +12,6 @@ const routes: Routes = [
   {
     path: 'add',
     component: AddRestoComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
   },
   {
     path: 'update/:id',
